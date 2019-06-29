bbb361fdfe58332b906ee185db5520269111d4dc
Robert Rinehart <rinebob111185@gmail.com>
2019-06-29 16:14:08 -0700

    added app,module and app.component files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+@Component({ 
+	selector: 'app-root',
+	template: `
+		<h1>Hello World</h1>
+		<h1>{{ title }}</h1>
+	`
 })
-export class AppComponent {
-  title = 'first-ang-app';
+
+export class AppComponent implements OnInit {
+	title: string;
+	constructor() { }
+
+	ngOnInit() {
+		this.title = 'Yo hey dude using Data Binding';
+	}
+
 }
