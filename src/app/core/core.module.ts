b0957c94c26fd3293aaf34689a9d3910722ba792
Bob Rinehart <rinebob111185@gmail.com>
2019-07-02 09:49:10 -0700

    added httpClient to core.module.ts

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/htttp';
+import { HttpClientModule } from '@angular/common/http';
 
 import { DataService } from './data.service';
 import { SorterService } from './sorter.service';
 
 @NgModule({
-	imports: [ ],
+	imports: [ HttpClientModule ],
 	providers: [ DataService, SorterService ]
 })
 
